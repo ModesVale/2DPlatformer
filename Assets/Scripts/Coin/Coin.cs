c13d68e0b27fb357272a806d81fddad56065fa7f
ModesVale <alex7eremins@gmail.com>
2025-09-09 22:40:47 +0300

    init

--- Assets/Scripts/Coin/Coin.cs
+++ Assets/Scripts/Coin/Coin.cs
@@ -1,6 +1,31 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Coin : MonoBehaviour
 {
-    
+    private UnityAction _returnAction;
+    private bool _picked;
+
+    public void RegisterReturn(UnityAction returnAction)
+    {
+        _returnAction = returnAction;
+    }
+
+    public void ResetState()
+    {
+        _picked = false;
+        gameObject.SetActive(true);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out CharacterMove player))
+        {
+            if (_picked) return;
+
+            _picked = true;
+            gameObject.SetActive(false);
+            _returnAction?.Invoke();
+        }
+    }
 }
--- Assets/Scripts/Coin/CoinSpawner.cs
+++ Assets/Scripts/Coin/CoinSpawner.cs
@@ -1,18 +1,46 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class CoinSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Coin _prefab;
+    [SerializeField] private Transform[] _spawnPoints;
+    [SerializeField] private float _respawnDelay = 5f;
+    [SerializeField] private int _poolCapacity = 10;
+
+    private ObjectPool<Coin> _pool;
+
+    private void Awake()
     {
-        
+        _pool = new ObjectPool<Coin>(
+            createFunc: () => Instantiate(_prefab),
+            actionOnGet: (coin) => coin.ResetState(),
+            actionOnRelease: (coin) => coin.gameObject.SetActive(false),
+            actionOnDestroy: (coin) => Destroy(coin.gameObject),
+            collectionCheck: true,
+            defaultCapacity: _poolCapacity
+        );
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        foreach (Transform point in _spawnPoints)
+        {
+            SpawnAtPoint(point);
+        }
+    }
+
+    private void SpawnAtPoint(Transform point)
+    {
+        Coin coin = _pool.Get();
+        coin.transform.position = point.position;
+        coin.RegisterReturn(() => StartCoroutine(RespawnAtPoint(point)));
+    }
+
+    private IEnumerator RespawnAtPoint(Transform point)
+    {
+        yield return new WaitForSeconds( _respawnDelay );
+        SpawnAtPoint(point);
     }
 }
